e0b471794e41956d6cf37b5c8d28b06440566186
Luiz Mário <luizmariofontes@gmail.com>
2025-02-12 10:49:15 -0300

    Adicionando o total da compra

--- core/models/compra.py
+++ core/models/compra.py
@@ -21,14 +21,13 @@ class Compra(models.Model):
     usuario = models.ForeignKey(User, on_delete=models.PROTECT, related_name="compras")
     status = models.IntegerField(choices=StatusCompra.choices,  default=StatusCompra.CARRINHO)
     tipo_pagamento = models.IntegerField(choices=TipoPagamento.choices, default=TipoPagamento.CARTAO_CREDITO)
+    total = models.DecimalField(max_digits=10, decimal_places=2, default=0)
     data = models.DateTimeField(auto_now_add=True)
-    @property
-    def total(self):
-            # total = 0
-            # for item in self.itens.all():
-            #     total += item.livro.preco * item.quantidade
-            # return total
-            return sum(item.livro.preco * item.quantidade for item in self.itens.all())
+    
+    
+    def save(self, *args, **kwargs):
+        self.total = sum(item.preco * item.quantidade for item in self.itens.all())
+        super().save(*args, **kwargs)
 
 class ItensCompra(models.Model):
     compra = models.ForeignKey(Compra, on_delete=models.CASCADE, related_name="itens")
--- core/serializers/compra.py
+++ core/serializers/compra.py
@@ -42,7 +42,7 @@ class ItensCompraCreateUpdateSerializer(ModelSerializer):
 
 class CompraCreateUpdateSerializer(ModelSerializer):
     usuario = HiddenField(default=CurrentUserDefault())
-    itens = ItensCompraCreateUpdateSerializer(many=True) # Aqui mudou
+    itens = ItensCompraCreateUpdateSerializer(many=True)
 
     class Meta:
         model = Compra
@@ -50,21 +50,33 @@ class CompraCreateUpdateSerializer(ModelSerializer):
 
     def create(self, validated_data):
         itens = validated_data.pop("itens")
-        compra = Compra.objects.create(**validated_data)
+        usuario = validated_data["usuario"]
+
+        compra, criada = Compra.objects.get_or_create(
+            usuario=usuario, status=Compra.StatusCompra.CARRINHO, defaults=validated_data
+        )
+
         for item in itens:
-            item["preco"] = item["livro"].preco # nova linha
-            ItensCompra.objects.create(compra=compra, **item)
+            item_existente = compra.itens.filter(livro=item["livro"]).first()
+
+            if item_existente:
+                item_existente.quantidade += item["quantidade"]
+                item_existente.preco = item["livro"].preco
+                item_existente.save()
+            else:
+                item["preco"] = item["livro"].preco
+                ItensCompra.objects.create(compra=compra, **item)
         compra.save()
         return compra
-    
+
     def update(self, compra, validated_data):
-        itens = validated_data.pop("itens")
+        itens = validated_data.pop("itens", [])
         if itens:
             compra.itens.all().delete()
             for item in itens:
-                item["preco"] = item["livro"].preco  # nova linha
+                item["preco"] = item["livro"].preco
                 ItensCompra.objects.create(compra=compra, **item)
-        compra.save()
+
         return super().update(compra, validated_data)
     
 class ItensCompraListSerializer(ModelSerializer):
