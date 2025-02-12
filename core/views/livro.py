cdf509893c60b24f650061fd2b26c82de135843a
Luiz Mário <luizmariofontes@gmail.com>
2025-02-12 09:28:08 -0300

    Utilizando filtros

--- core/views/livro.py
+++ core/views/livro.py
@@ -6,11 +6,14 @@ from rest_framework.viewsets import ModelViewSet
 from core.serializers import LivroListSerializer, LivroSerializer, LivroRetrieveSerializer,   LivroAlterarPrecoSerializer, LivroAjustarEstoqueSerializer
 from django.db.models.aggregates import Sum
 from core.models import Livro
+from django_filters.rest_framework import DjangoFilterBackend
 
 
 class LivroViewSet(ModelViewSet):
     queryset = Livro.objects.all()
     serializer_class = LivroSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ["categoria__descricao", "editora__nome", "autor__nome", "categoria__nome", "livro__nome"]
 
     def get_serializer_class(self):
         if self.action == "list":
